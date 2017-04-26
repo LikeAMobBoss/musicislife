03767834db67e6ab533da6d82e64fca30b6a7dfe
Jason Tidwell <tidwell2726@ymail.com>
2017-04-26 16:10:29 -0600

    added everything

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(drawSongs);
   }
 
-  function drawSongs(songList){
+  function drawSongs(songList) {
     console.log(songList);
     // This is where you task begins
-      
+    var songs = document.getElementById("songlists")
+    var template = ""
+    for (var i = 0; i < 50; i++) {
+      template += `
+      <div class="row">
+      <div class="panel panel-invert">
+  <div class="panel-body">
     
-    
+      <div class="col-lg-3"><h4>${songList[i].title}</h4></div>
+<div class="col-lg-1"><img src= "${songList[i].albumArt}"></div>
+<div class="col-lg-2"><h4>${songList[i].artist}</h4></div>
+<div class="col-lg-2"><h4>${songList[i].collection}</h4></div>
+<div class="col-lg-1"><h4>${songList[i].price}</h4></div>
+<div class="col-lg-3"><h4><audio controls >
+          <source src="${songList[i].preview}" type="audio/ogg">
+          </audio></h4></div>
+  </div>
+</div>
+    </div>
+`
+
+      /*<h4>${songList[i].title}</h4>
+       <img src= "${songList[i].albumArt}">
+        <h4>${songList[i].artist}</h4>
+         <h4>${songList[i].collection}</h4>
+          <h4>${songList[i].price}</h4>
+           <h4><audio controls >
+           <source src="${songList[i].preview}" type="audio/ogg">
+           </audio></h4>
+           */
+
+
+    }
+    songs.innerHTML = template
   }
-  
+
 }
 
 
